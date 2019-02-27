6bef2e20249afb32ce5ebe99ed0cfec46fa486bb
Roman Palesika <roman.palesik@binomo.com>
2019-02-27 18:10:50 +0300

    add scene

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
-import { Scene, PerspectiveCamera, WebGLRenderer, DirectionalLight } from 'three';
-
-const bodyColor = "rgba(23, 17, 20. 1)";
+import { Scene, PerspectiveCamera, WebGLRenderer, DirectionalLight,
+  LineBasicMaterial, Geometry, Vector3, Line } from 'three';
 
 window.onload = () => {
-    const scene = new Scene();
-    const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
-    const renderer = new WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.set( 0, 0, 100 );
 
-    document.body.appendChild( renderer.domElement );
+  const renderer = new WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const material = new LineBasicMaterial( { color: 0x0000ff } );
+
+  const geometry = new Geometry();
+  geometry.vertices.push(new Vector3( -10, 0, 0) );
+  geometry.vertices.push(new Vector3( 0, 10, 0) );
+  geometry.vertices.push(new Vector3( 10, 0, 0) );
+
+  const line = new Line( geometry, material );
+
+  scene.add(line);
+
+  animate();
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+  }
+
 };
